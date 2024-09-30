c97ca6bd2db9fec2174fe03dbbbf50593d2e0dd5
Marina Vyatkina <marinavyatk74@mail.ru>
2024-09-30 20:31:17 +0400

    Change camera position

--- src/components/3d/product/scene.tsx
+++ src/components/3d/product/scene.tsx
@@ -21,7 +21,7 @@ export function Scene(props: ModelProps) {
     >
       <Canvas
         className={clsx(s.canvas, s.interactiveCanvas)}
-        camera={{ position: [-1.36, 1.73, 2.38], fov: 50 }}
+        camera={{ position: [-1.36, 1.73, 2.78], fov: 50 }}
       >
         <ambientLight intensity={0.5} />
         <directionalLight position={[-1, 10, -2]} intensity={1.7} castShadow />
