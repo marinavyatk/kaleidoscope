e8c6352e6c6effc557ee5a1530cdffc34d87d3da
Marina Vyatkina <marinavyatk74@mail.ru>
2024-10-18 20:48:17 +0400

    gltf model instead of glb

--- src/components/3d/boy/boy.tsx
+++ src/components/3d/boy/boy.tsx
@@ -11,7 +11,7 @@ export type ModelProps = {
 function Model(props: ModelProps) {
   const { containerRef } = props;
   const sceneRef = useRef();
-  const { scene, animations } = useGLTF('/boy.glb', true);
+  const { scene, animations } = useGLTF('/boy/untitled.gltf', true);
   const { actions, names } = useAnimations(animations, sceneRef);
 
   useEffect(() => {
