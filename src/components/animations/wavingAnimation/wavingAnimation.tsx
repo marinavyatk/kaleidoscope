cd99d893f65c3110548241532be66afc3f6c324d
Marina Vyatkina <marinavyatk74@mail.ru>
2024-11-08 18:06:53 +0400

    Delayed loading of images for waving animation

---
+++ src/components/animations/wavingAnimation/wavingAnimation.tsx
@@ -0,0 +1,24 @@
+import { Ref } from 'react';
+import { useIntersectionObserver } from '@/common/customHooks/useIntersectionObserver';
+import { usePreloadImages } from '@/common/customHooks/usePreloadImages';
+import { Animation } from '@/components/animations/animation';
+import s from '@/sections/main/mainSection.module.scss';
+
+type WavingAnimationProps = {
+  containerRef: Ref<HTMLDivElement>;
+};
+
+export const WavingAnimation = (props: WavingAnimationProps) => {
+  const { containerRef } = props;
+  const shouldPlayAnimation = useIntersectionObserver(containerRef, 0.3);
+  const images = usePreloadImages({ animation: 'waving', imgQty: 96, reverse: false });
+  return (
+    <>
+      {images.length && shouldPlayAnimation ? (
+        <Animation images={images} ms={40} loop />
+      ) : (
+        <div className={s.kids + ' fullContainer backgroundImg'} />
+      )}
+    </>
+  );
+};
--- src/sections/main/mainSection.tsx
+++ src/sections/main/mainSection.tsx
@@ -1,15 +1,13 @@
 import { Button } from '@/components/button/button';
 import s from './mainSection.module.scss';
 import { memo, useRef } from 'react';
-import { Animation } from '@/components/animations/animation';
 import { useIntersectionObserver } from '@/common/customHooks/useIntersectionObserver';
-import { usePreloadImages } from '@/common/customHooks/usePreloadImages';
 import { Nullable } from '@/common/types';
+import { WavingAnimation } from '@/components/animations/wavingAnimation/wavingAnimation';
 
 const MainSection = () => {
   const animationRef = useRef<Nullable<HTMLDivElement>>(null);
-  const shouldPlayAnimation = useIntersectionObserver(animationRef, 0.3);
-  const images = usePreloadImages({ animation: 'waving', imgQty: 96, reverse: false });
+  const isVisible = useIntersectionObserver(animationRef, 0.3, true);
 
   return (
     <section
@@ -19,11 +17,7 @@ const MainSection = () => {
       itemType='https://schema.org/Organization'
     >
       <div className={s.background + ' fullContainer backgroundImg'}></div>
-      {images.length && shouldPlayAnimation ? (
-        <Animation images={images} ms={40} loop />
-      ) : (
-        <div className={s.kids + ' fullContainer backgroundImg'} />
-      )}
+      {isVisible && <WavingAnimation containerRef={animationRef} />}
       <h1 itemProp='name'>Калейдоскоп ИГР</h1>
       <p>Производим уникальные изделия для развития спортивного будущего!</p>
       <Button as='a' href='#catalog'>
