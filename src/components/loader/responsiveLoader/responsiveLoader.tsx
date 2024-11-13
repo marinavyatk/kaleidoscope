9b41cef998f6d1d608634670d224d2024f8f2d37
Marina Vyatkina <marinavyatk74@mail.ru>
2024-11-13 15:02:35 +0400

    Changed visibility till useEffect work

--- src/components/loader/responsiveLoader/responsiveLoader.tsx
+++ src/components/loader/responsiveLoader/responsiveLoader.tsx
@@ -2,16 +2,25 @@ import { useScreenWidth } from '@/common/customHooks/useScreenWidth';
 import Image from 'next/image';
 import { Loader } from '@/components/loader/loader';
 import s from '@/components/3d/3d.module.scss';
+import { useEffect, useState } from 'react';
+import { clsx } from 'clsx';
 
 export const ResponsiveLoader = () => {
-  const isMobile = useScreenWidth(767);
+  const isMobile = useScreenWidth(768);
+  const [isWidthKnown, setWidthKnown] = useState(false);
+
+  useEffect(() => {
+    setWidthKnown(true);
+  }, []);
+
+  const className = !isWidthKnown ? s.hidden : '';
 
   return isMobile ? (
-    <div>
+    <div className={className}>
       <Image src={'/boy.webp'} alt={'boy'} fill quality={100} />
     </div>
   ) : (
-    <div className='fullWidthCentered'>
+    <div className={clsx('fullWidthCentered', className)}>
       <Loader className={s.loader} />
     </div>
   );
