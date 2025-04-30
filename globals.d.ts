5555a0a3002aee3d42be035af3056a8e7af83e46
Marina Vyatkina <marinavyatk74@mail.ru>
2025-04-30 19:05:14 +0400

    Recaptcha hoisting error fix

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,12 @@
+export interface ReCaptchaInstance {
+  ready: (cb: () => void) => void;
+  execute: (siteKey: string, options: { action: string }) => Promise<string>;
+}
+
 declare global {
   interface Window {
     grecaptcha: ReCaptchaInstance;
-    captchaOnLoad: () => void;
   }
 }
+
+export {};
